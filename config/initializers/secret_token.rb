15b927ceb2fe6efd53cf8711d458db486bc04ce9
Alejandra Gutierrez <miss_amisa@hotmail.com>
2014-01-09 18:49:15 -0600

    create project rails and config it

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TddSandwich::Application.config.secret_key_base = 'a6b882c3256a220f2f844f17c2be6a1ebd83d0f1006508070d78b1fdb264f6ccc52e3c67ba7fc2a3b6622b02eb993cc5fc89496d45b69826cfac96ad09a4d8f0'
